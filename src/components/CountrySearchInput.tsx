3d858cc5237a8114240f4a89bc263b7880d23b8a
Vivek Kumar <92210976+vivekkumar004@users.noreply.github.com>
2022-05-05 19:58:51 +0530

    Add files via upload

--- src/components/CountrySearchInput.tsx
+++ src/components/CountrySearchInput.tsx
@@ -1,1 +1,31 @@
+import React from 'react';
+import './CountrySearchInput.css';
+import {useNavigate} from "react-router-dom";
 
+
+const CountrySearchInput = ({Validate}:any)=>{
+    function InputValidator(event: React.ChangeEvent<HTMLInputElement>){
+        const inputValue=event.target.value;
+        localStorage.setItem('storeValue', inputValue.toLocaleLowerCase());
+
+        const inputSubmit=document.getElementById('btn');
+        const nonCharacters=/[0-9]|\W/g;
+        if(inputValue===""||inputValue.match(nonCharacters))
+        {
+           ( inputSubmit as HTMLButtonElement).disabled=true;
+        }
+        else{
+            ( inputSubmit as HTMLButtonElement).disabled=false;
+        }
+    }
+    const navigator=useNavigate();
+    return <div id="whole">
+<h2 id="heading">Country's Capital and Current Weather</h2>
+    <form onSubmit={()=>navigator('countryDetails')}>
+         <input placeholder='Country Name' onChange={InputValidator} id='searchBox' type='text'  />
+         <button id="btn" type="submit" disabled>Submit</button>
+    </form>
+</div>
+}
+
+export default CountrySearchInput;
